497a0f37a569d0b6c4d51f1fe0219c25fe0c0ba7
Leo Yun Tao <Leoyuntao@gmail.com>
2023-01-25 11:40:13 +0800

    init commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Automation_Website.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
