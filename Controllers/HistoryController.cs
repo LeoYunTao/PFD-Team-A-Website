aa7fc83415a0762cd134bbc2c3c67e09fd4f1b57
Leo Yun Tao <Leoyuntao@gmail.com>
2023-02-02 00:15:52 +0800

    done history page

--- Controllers/HistoryController.cs
+++ Controllers/HistoryController.cs
@@ -1,12 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Automation_Website.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace Automation_Website.Controllers
 {
     public class HistoryController : Controller
     {
-        public IActionResult Index()
+        ApiCall apiCall = new ApiCall();
+
+        public IActionResult Index(int id, string conclusion, string created)
         {
-            return View();
+            HistoryViewModel historyViewModel = new HistoryViewModel()
+            {
+                Conclusion = conclusion,
+                Created = created,
+            };
+
+            string? conclusionStr = conclusion != null ? conclusion.ToLower() : conclusion;
+            if (conclusionStr == "running")
+            {
+				conclusionStr = "in_progress";
+            }
+            else if (conclusionStr == "all")
+            {
+                conclusionStr = null;
+            }
+
+            WorkflowRuns workflowRuns = apiCall.GetWorkflowRuns(perPage: 10, pageNumber: id, status: conclusionStr, created: created);
+
+            historyViewModel.SWorkflowRuns = workflowRuns;
+
+            return View(historyViewModel);
         }
     }
 }
